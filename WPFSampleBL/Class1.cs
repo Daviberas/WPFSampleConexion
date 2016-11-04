b349da4e43a0c3c38cee40b11e533edd2db07b47
dbenitez <dbenitez@107-14>
2016-11-04 11:52:52 +0100

    Conexiones BD

---
+++ WPFSampleBL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSampleBL
+{
+    public class Class1
+    {
+    }
+}
---
+++ WPFSampleUI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WPFSampleBL;
+
+namespace WPFSampleUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            clsListadosBL lista = new clsListadosBL();
+
+            return View(lista.getListadoPersonasBL());
+        }
+    }
+}
